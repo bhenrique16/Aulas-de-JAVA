ea97096ffab8bbaf9215e5d8e31a0fb29a1c179a
bhenrique16 <bh.henrique16@gmail.com>
2024-04-25 22:15:13 -0300

    Saida de dados em java

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,5 @@ Teste.java,9\d\9da26bd03b93aef3ed3617768fac9d07f557232d
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+>
+src/aula1.java,4\f\4fd1b955c1803086470b3144365e76df415831dc
---
+++ src/Aula1.java
@@ -0,0 +1,20 @@
+public class Aula1{
+
+    public static void main(String[] args) {
+        double x = 10.35784;
+        String nome = "Maria";
+        int idade = 10;
+        double renda = 4000.0;
+
+        System.out.println(x);
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.4f%n", x);
+
+        System.out.printf("%.4f%n", x);
+
+        System.out.println("RESULTADO = " + x + " METROS");
+        System.out.printf("RESULTADO = %.2f metros%n", x, x);
+
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+    }
+}
